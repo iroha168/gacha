1fdd06d249cd2ab5b77ded2e701b8fbb382a7c74
iroha168 <cnoizecehiky@yahoo.co.jp>
2017-10-21 21:53:34 +0900

    [MODIFY] let actual mails to be avaliable too

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ import nose.tools as nose
 import string
 import random
 import csv
-
+from datetime import datetime
 class Gacha:
     def exe(self):
         self.wait.until(ec.presence_of_all_elements_located)
         prize_img = self.driver.find_element_by_id('won_the_prize_img')
         next_img = prize_img.get_attribute('src')
+        self.driver.save_screenshot(datetime.now().strftime('%Y %m %d %H:%M:%S'))
         if next_img + '\n' not in self.nimgs:
             self.nimgs.append(next_img.strip() + '\n')
 
@@ -34,7 +35,6 @@ class Gacha:
             self.driver = webdriver.PhantomJS(executable_path=pjs_path, desired_capabilities=dcap)
             url = 'https://ragnarok.skylinematrix.com/lot/lottery/lottery.html'
             self.driver.get(url)
-
             self.wait = WebDriverWait(self.driver, 5)
             self.wait.until(ec.presence_of_all_elements_located)
 
@@ -51,27 +51,40 @@ class Gacha:
             self.wait.until(ec.presence_of_all_elements_located)
             btn.click()
             self.wait.until(ec.presence_of_all_elements_located)
-            onemore = self.driver.find_element_by_id('won_the_prize_button_continue')
 
             with open('img.txt', 'r') as f:
                 self.nimgs = f.readlines()
 
-            for i in range(10):
-                """
+            for i in range(35):
+                st = self.driver.find_element_by_id('share_tips')
                 if i == 10 or ( i > 10 and i % 5 == 0):
-                    self.driver.find_element_by_css_selector('.tc_share > p:nth-child(1)')
+                    #share = self.driver.find_element_by_css_selector('.tc_share > p:nth-child(1)')
+                    share = self.driver.find_element_by_css_selector('#lottery_button_share1')
+                    try:
+                        share.click()
+                    except Exception:
+                        print('cant share')
+                        break
                     self.wait.until(ec.presence_of_all_elements_located)
-                    self.driver.find_element_by_css_selector('.tc_close')
+                    self.driver.get(url)
                     self.wait.until(ec.presence_of_all_elements_located)
+                    onemore = self.driver.find_element_by_id('won_the_prize_button_continue')
+                    #self.driver.find_element_by_css_selector('.tc_close')
+                    self.wait.until(ec.presence_of_all_elements_located)
+                    btn = self.driver.find_element_by_class_name('yu_yue')
                     btn.click()
-                    self.wait.until(ec.presence_of_all_elements_located)
-                    print(i)
-                """
+                else:
+                    onemore = self.driver.find_element_by_id('won_the_prize_button_continue')
                 try:
                     onemore.click()
+                    print(str(i) + ': success')
                 except Exception:
-                    break
+                    print(str(i) + ': some kind exception happened')
+                    continue
                 if(self.exe()):
+                    done = self.driver.find_element_by_id('won_the_prize_button_OK')
+                    done.click()
+                    self.wait.until(ec.presence_of_all_elements_located)
                     return
 
             f.close()
